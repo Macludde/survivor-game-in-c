f93172f8696c2e87ce95fc27fc913704e0e2b3f3
Ludvig Svedberg <ludvig.svedberg@mac.com>
2025-01-04 21:59:57 +0100

    Add camera and trees

--- src/main.c
+++ src/main.c
@@ -8,6 +8,8 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 */
 
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "raylib.h"
 #include "raymath.h"
 
@@ -16,6 +18,7 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #define DEFAULT_SCREEN_WIDTH 1280
 #define DEFAULT_SCREEN_HEIGHT 800
 #define GAME_NAME "SurvivorGame"
+#define NUMBER_OF_TREES 20
 
 float playerSpeed = 600;
 Vector2 currentPlayerMovementDelta;
@@ -84,6 +87,39 @@ void HandleDebuggingKeys()
 	}
 }
 
+void HandleScreenResizing(Camera2D *camera)
+{
+	static int screenWidth;
+	static int screenHeight;
+	if (IsKeyPressed(KEY_F11))
+		ToggleFullscreen();
+	if (GetScreenWidth() != screenWidth || GetScreenHeight() != screenHeight)
+	{
+		screenWidth = GetScreenWidth();
+		screenHeight = GetScreenHeight();
+		camera->offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
+	}
+}
+
+#define CAMERA_MOVEMENT_SPEED 2.0f
+void MoveCameraToPlayer(Camera2D *camera, Vector2 playerPos)
+{
+	camera->target = Vector2Lerp(camera->target, playerPos, CAMERA_MOVEMENT_SPEED * GetFrameTime());
+}
+void ZoomCamera(Camera2D *camera)
+{
+	camera->zoom += ((float)GetMouseWheelMove() * 0.05f);
+	if (camera->zoom > 3.0f)
+		camera->zoom = 3.0f;
+	else if (camera->zoom < 0.1f)
+		camera->zoom = 0.1f;
+}
+void HandleCamera(Camera2D *camera, Vector2 playerPos)
+{
+	MoveCameraToPlayer(camera, playerPos);
+	ZoomCamera(camera);
+}
+
 unsigned int CONFIG_FLAGS = FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI | FLAG_MSAA_4X_HINT | FLAG_WINDOW_RESIZABLE | FLAG_WINDOW_ALWAYS_RUN;
 int main()
 {
@@ -93,33 +129,55 @@ int main()
 	InitWindow(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT, GAME_NAME);
 
 	Vector2 playerPos;
+	Vector2 *trees = malloc(NUMBER_OF_TREES * sizeof(Vector2));
+	if (trees == NULL)
+	{
+		printf("Failed to allocate memory for trees\n");
+		exit(1);
+	}
+	for (int i = 0; i < NUMBER_OF_TREES; ++i)
+	{
+		trees[i] = (Vector2){GetRandomValue(-DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_WIDTH), GetRandomValue(-DEFAULT_SCREEN_HEIGHT, DEFAULT_SCREEN_HEIGHT)};
+		for (int j = 0; j < i; ++j)
+		{
+			if (Vector2Distance(trees[i], trees[j]) < 120)
+			{
+				--i;
+				break;
+			}
+		}
+	}
 
-	// logic
-	int screenWidth = GetScreenWidth();
-	int screenHeight = GetScreenHeight();
-	playerPos.x = screenWidth / 2;
-	playerPos.y = screenHeight / 2;
+	Camera2D camera = {
+		.offset = {DEFAULT_SCREEN_WIDTH / 2.0f, DEFAULT_SCREEN_HEIGHT / 2.0f},
+		.target = playerPos,
+		.rotation = 0.0f,
+		.zoom = 1.0f,
+	};
 
 	// game loop
 	while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
-		if (GetScreenWidth() != screenWidth || GetScreenHeight() != screenHeight)
-		{
-			screenWidth = GetScreenWidth();
-			screenHeight = GetScreenHeight();
-			playerPos.x = screenWidth / 2;
-			playerPos.y = screenHeight / 2;
-		}
+		HandleScreenResizing(&camera);
 		HandleDebuggingKeys();
 
 		playerPos = GetNewPlayerPos(playerPos);
+		HandleCamera(&camera, playerPos);
 
 		// drawing
 		BeginDrawing();
-		DrawText(TextFormat("FPS: %i, TARGET: %i", GetFPS(), targetFps), 10, 10, 20, DARKGRAY);
-
 		ClearBackground(WHITE);
+
+		BeginMode2D(camera);
+		for (int i = 0; i < NUMBER_OF_TREES; i++)
+		{
+			DrawRectangle(trees[i].x, trees[i].y, 20, 40, BROWN);
+			DrawCircle(trees[i].x + 10, trees[i].y - 10, 30, GREEN);
+		}
 		DrawCircle(playerPos.x, playerPos.y, 20, DARKGREEN);
+		EndMode2D();
+
+		DrawText(TextFormat("FPS: %i, TARGET: %i", GetFPS(), targetFps), 10, 10, 20, DARKGRAY);
 
 		// end the frame and get ready for the next one  (display frame, poll input, etc...)
 		EndDrawing();
